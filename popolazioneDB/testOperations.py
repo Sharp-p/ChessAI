418721190c8fa29ab2c57c6daf45d83bb2f4adfc
Sharp-p <thomassharp2003@gmail.com>
2024-06-21 23:31:43 +0200

    Codice pronto per popolare il db

--- popolazioneDB/testOperations.py
+++ popolazioneDB/testOperations.py
@@ -1,12 +1,17 @@
 import sqlite3
 
-
-#====== SELECT ======
-
 con= sqlite3.connect("ChessPositions.db")
 cur= con.cursor()
 
-cur.execute("SELECT * FROM position")
+#====== SELECT ======
+
+""" cur.execute("SELECT * FROM position")
 for row in cur: print(row)
+ """
+#====== DELETE ======
+
+cur.execute("DELETE FROM position")
+con.commit()
+
 
 con.close()
